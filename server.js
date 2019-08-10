58af7e2fe8f2afc28c2ba7b484d1f23aa6b762b2
Jimi Loc Nguyen <jimilocn@gmail.com>
2019-08-10 14:00:27 -0500

    completed routing to pink ranger html page when using node server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 var express = require("express");
-var exphbs = require("express-handlebars");
+
 
 var db = require("./models");
 
@@ -12,14 +12,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
