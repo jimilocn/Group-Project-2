967357bc6ab38440e3257d2fec2f20007a44bf8f
jenineellis <jenine.ellis@gmail.com>
2019-08-10 14:19:12 -0500

    pushing to my branch to keep changes

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:8080');
+var socket = io.connect('http://localhost:3000');
 var io = require('socket.io')(http);
 
 
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 var express = require("express");
-var exphbs = require("express-handlebars");
-
 var db = require("./models");
+
 
 var app = express();
 var PORT = process.env.PORT || 3000;
